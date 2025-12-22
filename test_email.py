1dfa30fee2e77ffb8371294da7863fb62ece1a23
Pinki Dagar <151912705+pinkidagar18@users.noreply.github.com>
2025-12-22 20:28:59 +0530

    Update test_email.py

--- test_email.py
+++ test_email.py
@@ -1,3 +1,3 @@
 from app import send_email
 
-send_email("pinkidagar18@gmail.com", "pinkidagar12110202@gmail.com", "<h1>This is a email from SaarthiAI Pinki dagar you have less attendence</h1>")
+send_email("youemail@gmail.com", "someone@gmail.com", "<h1>This is a email from SaarthiAI Pinki dagar you have less attendence</h1>")
